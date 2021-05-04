2254e80d0010223ebd2440c5eaa70141a0a54007
lalalazxcxd <lancelemuel.morona@gmail.com>
2021-05-05 06:21:17 +0800

    Initial Setup

--- server.js
+++ server.js
@@ -25,4 +25,29 @@ db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
 
-app.listen(process.env.PORT || 3000)
+const server = app.listen(process.env.PORT || 3000, () => {
+    console.log("Server is listening on port 3000")
+})
+
+const io = require('socket.io')(server)
+
+io.on('connection', (socket) =>{
+    console.log("A new client has been connected");
+
+socket.username = "Anonymous";
+
+    socket.on('new_message',(data) => {
+        io.sockets.emit('new_message', {
+            message: data.message,
+            username: socket.username
+        });
+
+    socket.on("change_username", data =>{
+        socket.username = data.username;
+    });
+
+    socket.on("typing", (data)=>{
+        socket.broadcast.emit("typing",{username:socket.username});
+    });
+    });
+});
